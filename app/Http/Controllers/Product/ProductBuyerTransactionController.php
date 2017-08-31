9847299ea2f01223adeadb247d372b5a98128be8
Mayur <mayur.pathak52@gmail.com>
2017-08-31 11:44:47 +0530

    implemented other controllers, now implementing store method for productBuyerTransaction

---
+++ app/Http/Controllers/Product/ProductBuyerTransactionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class ProductBuyerTransactionController extends ApiController
+{
+  
+
+    public function store(Request $request)
+    {
+        
+    }
+
+   
+}
--- routes/api.php
+++ routes/api.php
@@ -34,12 +34,16 @@ Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only'
 
 /*Products*/
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 
 /*Sellers*/
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index', 'show']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
 Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
 
 
 /*Transactions*/
